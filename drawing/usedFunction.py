e890ef063e5ef88d0e5eb9397b2c52dd98369161
chicheng567 <samuel910617@yahoo.com.tw>
2022-07-31 09:52:46 +0800

    static setting

--- drawing/usedFunction.py
+++ drawing/usedFunction.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 def handle_upload_file(f):
+    
     return 
--- drawing/views.py
+++ drawing/views.py
@@ -1,2 +1,10 @@
+from re import L
 from django.shortcuts import render
+from django.http import JsonResponse
 # Create your views here.
+def home(request):
+    content = {}
+    content['test'] = '5678392342'
+    content['date'] = '2022/7/20'
+    content['Bool'] = -2
+    return render(request, 'home.html', content)
